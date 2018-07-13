cfc4b2ebc41446d5896f6824794611dc6f396d47
Jacek Klosinski <jklosinski@bluesoft.net.pl>
2018-07-13 15:29:13 +0200

    MVC Layout

---
+++ PrzyjaznePrzedszkole/PrzyjaznePrzedszkole/Controllers/AppController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrzyjaznePrzedszkole.Controllers
+{
+    public class AppController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("notice-board")]
+        public IActionResult NoticeBoard()
+        {
+            ViewBag.Title = "Notice Board";
+            return View();
+        }
+
+        [HttpGet("messages")]
+        public IActionResult Messages()
+        {
+            ViewBag.Title = "Messages";
+            return View();
+        }
+
+        [HttpGet("photos")]
+        public IActionResult Photos()
+        {
+            ViewBag.Title = "Photos";
+            return View();
+        }
+
+        [HttpGet("for-parents")]
+        public IActionResult ForParents()
+        {
+            ViewBag.Title = "ForParents";
+            return View();
+        }
+    }
+}
