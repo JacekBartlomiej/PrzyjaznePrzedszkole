9dfd6acdd3af4d99b6a477536db87969d823206f
Jacek Klosinski <jklosinski@bluesoft.net.pl>
2018-07-18 09:20:59 +0200

    RWD - CSS Preprocessor setup

---
+++ PrzyjaznePrzedszkole/PrzyjaznePrzedszkole/ViewModels/NoticeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrzyjaznePrzedszkole.ViewModels
+{
+    public class NoticeViewModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Audience { get; set; }
+        public string Message { get; set; }
+    }
+}
--- PrzyjaznePrzedszkole/PrzyjaznePrzedszkole/wwwroot/js/script.js
+++ PrzyjaznePrzedszkole/PrzyjaznePrzedszkole/wwwroot/js/script.js
@@ -1,7 +1,15 @@
 ﻿if (window.location.pathname == '/notice-board') {
     let addNoticeBtn = document.getElementById("addNoticeBtn");
     let seeNoticeBtn = document.getElementById("seeNoticeBtn");
-    console.log(addNoticeBtn);
     addNoticeBtn.addEventListener("click", function () { location.replace("/add-notice") });
     seeNoticeBtn.addEventListener("click", function () { location.replace("/notice") });
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
