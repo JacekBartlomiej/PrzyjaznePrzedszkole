30e2ff8ad70bc783bde46072d0c83166a74aeb11
Jacek Klosinski <jacek.klosinski@bluesoft.com>
2019-07-22 20:09:43 +0200

    Adding preview to create gallery component

--- PrzyjaznePrzedszkole/PrzyjaznePrzedszkole/ClientApp/app/gallery/create-gallery/create-gallery.component.ts
+++ PrzyjaznePrzedszkole/PrzyjaznePrzedszkole/ClientApp/app/gallery/create-gallery/create-gallery.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateGalleryComponent implements OnInit {
 
+  urls = new Array<string>();
+
+  detectFiles(event) {
+    this.urls = [];
+    let files = event.target.files;
+    if (files) {
+      for (let file of files) {
+        let reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.urls.push(e.target.result);
+        }
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
